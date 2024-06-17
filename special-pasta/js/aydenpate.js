7d6b996c1a740a6270fea584f8f2e928cd920aa7
Leo-Turible <turibleleo@gmail.com>
2024-06-17 15:09:31 +0200

    Ajout du moyen de paiment [FIXED]

--- special-pasta/js/aydenpate.js
+++ special-pasta/js/aydenpate.js
@@ -117,7 +117,7 @@ jQuery(document).ready(function($) {
         $.post(aydenpate_data.ajax_url, formData, function(response) {
             if (response.success) {
                 alert('Produit ajouté au panier');
-                window.location.href = '/tpwp3/cart/';
+                window.location.href = '/cart/';
             } else {
                 alert('Erreur lors de l\'ajout au panier : ' + response.data.message);
                 console.log(response.data);
